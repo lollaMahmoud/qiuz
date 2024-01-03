9b6b71693ccbcff6c5c2cb47511020ddc0e5d841
lollaMahmoud <flermeme123@gmali.com>
2024-01-03 10:40:26 +0200

    the project

---
+++ home.js
@@ -0,0 +1,15 @@
+function go()
+{
+    location.replace("forms.html")
+}
+
+
+function goshopping()
+{
+    location.replace("shopping.html")
+}
+
+function gohome()
+{
+    location.replace("home.html")
+}
